914f999505479840e0d3675e5967519489154053
Pinich Yevgen <evpinich@gmail.com>
2017-09-10 22:56:56 +0300

    fix error and optimization come

--- calculator.py
+++ calculator.py
@@ -1,103 +1,83 @@
+#!/usr/bin/python
 class Calculator(object):
+    """"Class of calculating text expression  """
+    _error_state = False
+    _error_masage = set()
 
-    __errorState  = False
-    __errorMasage = set()
- 
-    """"Method for calculating expression  -------------------------------------------------------------------------"""  
-    def calcExpression(self,expression):
-        self.__errorStatus=False
-        self.__errorMasage=set()
-        
-        proxyExpression = expression.strip(" \t\n")
-        countLeftBrackets=0
-        countRightBrackets=0
-        for symbol in proxyExpression:
-            if (symbol==")"):countLeftBrackets += 1
-            if (symbol=="("):countRightBrackets += 1
-        if not(countLeftBrackets==countRightBrackets):
-            self.__errorStatus=True;
-            self.__errorMasage.add("ERROR: обнаружены непарные скопки внутри выражения")
+    def _calc_expression(self, expression):
+        """"Method for calculating expression  """
+        self._error_status = False
+        self._error_masage = set()
+        proxy_expression = expression.strip(" \t\n")
+        count_left_brackets = proxy_expression.count("(")
+        count_right_brackets = proxy_expression.count(")")
+        if count_left_brackets != count_right_brackets:
+            self._error_status = True
+            self._error_masage.add("ERROR: обнаружены непарные скопки внутри выражения")
+        else:
+            try:
+                while "(" in proxy_expression:
+                    lexem = "(" + self._find_lexem_lnside_expression(proxy_expression) + ")"
+                    result_of_lexem = str(self._calc_lexem(lexem))
+                    proxy_expression = proxy_expression.replace(lexem, result_of_lexem)
+                result = self._calc_lexem(proxy_expression)
+            except BaseException:
+                self._error_status = True
+                self._error_masage.add("ERROR: обнаружена неизвесная синтаксическая ошибка")
+        if (self._error_status):
+            return self._error_masage
+        return result
 
-        try:
-            while "(" in proxyExpression:
-                lexem = "("+self.__findLexemInsideExpression(proxyExpression)+")"
-                resultOfLexem = str(self.__calcLexem(lexem) )
-                proxyExpression=proxyExpression.replace(lexem,resultOfLexem)      
-            result=self.__calcLexem(proxyExpression)
-        except:
-              self.__errorStatus=True;
-              self.__errorMasage.add("ERROR: обнаружена неизвестная синтаксическая ошибка")      
-        if (self.__errorStatus) : return self.__errorMasage
-        else: return result
-      
-    """"Method that doing calculation of simple lexem  -------------------------------------------------------------"""
-    def __calcLexem(self,lexem):
+    def _calc_lexem(self, lexem):
+        """"Method that doing calculation of simple lexem  ------------"""
         list_element = []
-        symbolBufer = ""
+        symbol_bufer = ""
         for symbol in lexem:
-            if (not(symbol in "()-+*/.1234567890")):
-                self.__errorStatus=True
-                self.__errorMasage.add("ERROR: обнаружен недопустимый символ внутри выражения")
+            if symbol not in "()-+*/.1234567890":
+                self._error_status = True
+                self._error_masage.add("ERROR: обнаружен недопустимый символ внутри выражения")
                 return 0
-            if (len(symbolBufer)>0) and (symbol in  "+-"):
-                list_element.append(symbolBufer)
-                symbolBufer=""
-            if (symbol in  "*/"):
-                list_element.append(symbolBufer)
+            if (len(symbol_bufer) > 0) and (symbol in "+-"):
+                list_element.append(symbol_bufer)
+                symbol_bufer = ""
+            if (symbol in "*/"):
+                list_element.append(symbol_bufer)
                 list_element.append(symbol)
-                symbolBufer=""
-            if symbol in  "-+1234567890.":
-                symbolBufer=symbolBufer+symbol         
-        list_element.append(symbolBufer)
-        
-        for i in range(len(list_element)-1):
-            if (i>0) and (list_element[i]=="*")and (len(list_element)):
-                list_element[i+1]=str(float(list_element[i-1])*float(list_element[i+1]))
-                list_element[i-1]="0"
-                list_element[i]="0"
-            if (i>0) and (list_element[i]=="/")and (len(list_element)):
+                symbol_bufer = ""
+            if symbol in "-+1234567890.":
+                symbol_bufer += symbol
+        list_element.append(symbol_bufer)
+        for i in range(len(list_element) - 1):
+            if (i > 0) and (list_element[i] == "*")and (len(list_element)):
+                list_element[i + 1] = str(float(list_element[i - 1]) * float(list_element[i + 1]))
+                list_element[i - 1] = "0"
+                list_element[i] = "0"
+            if (i > 0) and (list_element[i] == "/")and (len(list_element)):
                 try:
-                    list_element[i+1]=str(float(list_element[i-1])/float(list_element[i+1]))
+                    list_element[i + 1] = str(float(list_element[i - 1]) / float(list_element[i + 1]))
                 except ZeroDivisionError:
-                    self.__errorStatus=True;
-                    self.__errorMasage.add("ERROR: обнаружено деление на ноль внутри выражения")
+                    self._error_status = True
+                    self._error_masage.add("ERROR: обнаружено деление на ноль внутри выражения")
+                list_element[i - 1] = "0"
+                list_element[i] = "0"
+        ansver = 0
 
-                list_element[i-1]="0"
-                list_element[i]="0"
-        ansver=0
-        
         for i in range(len(list_element)):
-           ansver=ansver+float( list_element[i])
+            ansver = ansver + float(list_element[i])
         return ansver
-    
-    """"Method that extractoring simple lexem from expression  -----------------------------------------------------"""
-    def __findLexemInsideExpression(self,expression):
-        lexem=""
-        countOpeningBrackets=0
-        countClosingBrackets=0
-        beginIndexLem=0
-        endIndexLem=0
-        counter=0
-        lexemFieldFlag=False
+
+    def _find_lexem_lnside_expression(self, expression):
+        """"Method that extractoring simple lexem from expression  --------"""
+        counter, lexem = 0, ""
+        begin_index_lexem, end_index_lexem, lexem_field_flag = 0, 0, False
         for i in expression:
-            counter +=1
-            if (i=="("):
-                lexemFieldFlag=True
-                beginIndexLem=counter
-                countOpeningBrackets += 1
-            if  (lexemFieldFlag==False) and ( i==")"):
-                countClosingBrackets += 1
-            if  lexemFieldFlag and ( i==")"):
-                lexemFieldFlag=False
-                endIndexLem=counter-1
-                countClosingBrackets += 1
-                lexem=expression[beginIndexLem:endIndexLem]
+            counter += 1
+            if (i == "("):
+                lexem_field_flag = True
+                begin_index_lexem = counter
+            elif lexem_field_flag and (i == ")"):
+                lexem_field_flag = False
+                end_index_lexem = counter - 1
+                lexem = expression[begin_index_lexem:end_index_lexem]
                 break
         return lexem
-
-
-ClassCalc=Calculator()    
-user_expression=input("Введите выражение в формате ПРИМЕР: 23-(34*34)/(30-5)+23-(34*34)/(30-5)\n")
-print("*******************************************************")
-print("ОТВЕТ=",ClassCalc.calcExpression(user_expression))
-print("*******************************************************")
